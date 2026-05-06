8280987e00061e1e8f51f1cf276d14f6b15497c7
afifurrohman <jack4our125@gmail.com>
2026-05-06 11:13:52 +0700

    Add files via upload

---
+++ materiarray 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string hari[7]={"minggu","senin","selasa","rabu","kamis","jumat","sabtu"};
+	cout<<"Nama-nama hari:\n";
+//	cout<<hari[0]<<endl;
+//	cout<<hari[1]<<endl;
+//	cout<<hari[2]<<endl;
+//	cout<<hari[3]<<endl;
+//	cout<<hari[4]<<endl;
+//	cout<<hari[5]<<endl;
+//	cout<<hari[6 ]<<endl;
+	for(int i=0;i<7;i++){
+		cout<<hari[i]<<endl;
+	}
+	
+	
+}
